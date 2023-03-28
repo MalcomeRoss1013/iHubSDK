b1e27d7a0663b7fe3dc9556d0c5f7735e2d4f2f8
Divail <Fargon847@gmail.com>
2023-03-28 12:53:02 -0500

    Dino assets
    dinosaur assets added

---
+++ My project/Assets/MobileDinosaurPack/3D_Meshes/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject food;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Disappear()
+    {
+        food.GetComponent<Renderer>().enabled = false;
+    }
+}
